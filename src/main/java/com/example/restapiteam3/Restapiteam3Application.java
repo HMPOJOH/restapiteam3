0748577264b0e28df9316ab71c5d961749b03782
HM\POJOH <pontus.johansson@hm.com>
2020-12-03 11:26:59 +0100

    added template with spring web + thymeleaf

---
+++ src/main/java/com/example/restapiteam3/Restapiteam3Application.java
@@ -0,0 +1,13 @@
+package com.example.restapiteam3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Restapiteam3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Restapiteam3Application.class, args);
+	}
+
+}
