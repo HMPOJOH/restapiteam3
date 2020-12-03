022008d7605d91d5b74440c82f8228656a8dfa5e
HM\POJOH <pontus.johansson@hm.com>
2020-12-03 11:59:03 +0100

    added a class

---
+++ src/main/java/com/example/employee/Employee.java
@@ -0,0 +1,23 @@
+package com.example.employee;
+
+public class Employee {
+
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String SSN;
+    private Integer salary;
+    private Boolean active;
+    private String startDate;
+
+
+
+    /* "id": Number,
+            "firstName": String,
+            "lastName": String,
+            "SSN": String,
+            "department": String,
+            "salary": Number,
+            "active": Boolean,
+            "startDate": String*/
+}
